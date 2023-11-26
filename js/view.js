2517fdf92400e3ef38428532ae237c3fa796c9c6
labicquette <komete17@gmail.com>
2023-11-26 22:14:07 +0100

    modifs

--- js/view.js
+++ js/view.js
@@ -13,25 +13,32 @@ class View {
 
     this.row_start_inner = document.createElement('div')
     this.row_center_inner = document.createElement('div')
-    this.row_end_inner= document.createElement('div')
 
     this.left = document.createElement('div');
     this.right = document.createElement('div')
     this.center = document.createElement('div')
-    
+    this.center.textContent = "AffluNantes"
+    this.center_title = document.createElement('div')
+    this.center_title.textContent = "Commerce"
+
+  
     this.left2 = document.createElement('div');
     this.right2 = document.createElement('div')
     this.center2 = document.createElement('div')
+  
 
     //this.div.appendChild();
-    this.container = document.createElement('div')
+    this.bot_left_but = document.createElement('button');
+    this.bot_cent_left_but = document.createElement('button')
+    this.bot_cent_right_but = document.createElement('button')
+    this.bot_right_but = document.createElement('button')
+
+    this.go_back_but = document.createElement('button')
+    this.go_back_but.textContent = "<-"
 
     this.plus = document.createElement('button');
     this.moins = document.createElement('button')
     this.text = document.createElement('input')
-
-    this.end_text = document.createElement('textarea')
-    this.end_text.textContent='end AREA'
 
     this.plus.textContent= "+"
     this.moins.textContent='-'
@@ -47,25 +54,41 @@ class View {
     // BOOTSTRAP
     // ------------------------------------------
 
-    this.container.classList.add("container-fluid")
-
     this.moins.classList.add("btn","btn-primary", "btn-lg");
     this.plus.classList.add("btn")
 
     this.but1.classList.add('btn')
     this.but2.classList.add('btn')
 
-    this.row_start.classList.add("row_start")
+
+    this.go_back_but.classList.add('btn', 'go_back')
+
+    //START
+    this.row_start.classList.add("row", "top")
+    this.row_start.setAttribute("id", "top")
     this.row_start_inner.classList.add("row")
     
-    this.row_center.classList.add("row_center")
-    this.row_center_inner.classList.add("row")
+    //CENTER
+    this.row_center.classList.add("row", "flex-fill", "inner_body")
+    this.row_center.setAttribute("id", "inner_body")
 
-    this.row_end.classList.add("row_end")
-    this.row_end_inner.classList.add("row")
+    this.row_center_inner.classList.add("row")
+    
+
+
+    this.center_title.classList.add('col', 'station_title')
+
+    //END
+    this.row_end.classList.add("row", "bottom_menu", 'd-flex', 'btn-toolbar')
+    this.row_end.setAttribute("id", "bottom_menu")
+
+    this.bot_left_but.classList.add('btn', 'btn-primary', 'col', 'margin_right')
+    this.bot_cent_left_but.classList.add('btn',  'btn-primary','col',  'margin_right')
+    this.bot_cent_right_but.classList.add('btn', 'btn-primary', 'col',  'margin_right')
+    this.bot_right_but.classList.add('btn', 'btn-primary', 'col', 'right_button')
 
     this.left.classList.add("col")
-    this.center.classList.add("col")
+    this.center.classList.add("col", 'title')
     this.right.classList.add("col")
 
     this.left2.classList.add("col")
@@ -75,32 +98,38 @@ class View {
     // ------------------------------------------
     // CREATING TREE
     // ------------------------------------------
-    nodeParent.appendChild(this.container)
+    
 
-    this.container.appendChild(this.row_start);
-    this.container.appendChild(this.row_center);
-    this.container.appendChild(this.row_end);
+    nodeParent.appendChild(this.row_start);
+    nodeParent.appendChild(this.row_center);
+    nodeParent.appendChild(this.row_end);
 
     this.row_start.appendChild(this.left);
     this.row_start.appendChild(this.center);
     this.row_start.appendChild(this.right)
 
-    this.row_start.appendChild(this.row_start_inner);
-    this.row_center.appendChild(this.row_center_inner);
-    this.row_end.appendChild(this.row_end_inner)
+    this.left.appendChild(this.go_back_but)
 
-    this.center.appendChild(this.moins)
-    this.center.appendChild(this.plus)
-    this.center.appendChild(this.text)
+    this.row_center.appendChild(this.row_center_inner);
+    this.row_center_inner.appendChild(this.center_title);
+
+
+    this.center2.appendChild(this.moins)
+    this.center2.appendChild(this.plus)
+    this.center2.appendChild(this.text)
 
     this.row_center.appendChild(this.left2);
     this.row_center.appendChild(this.center2);
     this.row_center.appendChild(this.right2)
 
+    this.row_end.appendChild(this.bot_left_but)
+    this.row_end.appendChild(this.bot_cent_left_but)
+    this.row_end.appendChild(this.bot_cent_right_but)
+    this.row_end.appendChild(this.bot_right_but)
+
     this.left2.appendChild(this.but1)
     this.right2.appendChild(this.but2)
-   
-    this.row_end.appendChild(this.end_text)
+  
     
 
 
